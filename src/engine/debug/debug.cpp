6008443fbe55b34d405a0a51d3f68a19c842c9e1
Vid Furlan <nalrufdiv@gmail.com>
2025-08-19 19:01:41 +0200

    Disable debug ansi on windows

--- src/engine/debug/debug.cpp
+++ src/engine/debug/debug.cpp
@@ -7,14 +7,25 @@ namespace engine::debug {
 
 		const char* levelToString(Level level)
 		{
+#ifdef _WIN32
 			switch (level) {
-			case Level::Info:  return "[\e[1;37m Info  \e[0m]";
-			case Level::Debug: return "[\e[1;32m Debug \e[0m]";
-			case Level::Warn:  return "[\e[1;33m Warn  \e[0m]";
-			case Level::Error: return "[\e[1;31m Error \e[0m]";
-			case Level::Fatal: return "[\e[1;91m Fatal \e[0m]";
+				case Level::Info:  return "[ Info  ]";
+				case Level::Debug: return "[ Debug ]";
+				case Level::Warn:  return "[ Warn  ]";
+				case Level::Error: return "[ Error ]";
+				case Level::Fatal: return "[ Fatal ]";
 			}
 			return "[\e[1;37m   ?   \e[0m]";
+#else
+			switch (level) {
+				case Level::Info:  return "[\e[1;37m Info  \e[0m]";
+				case Level::Debug: return "[\e[1;32m Debug \e[0m]";
+				case Level::Warn:  return "[\e[1;33m Warn  \e[0m]";
+				case Level::Error: return "[\e[1;31m Error \e[0m]";
+				case Level::Fatal: return "[\e[1;91m Fatal \e[0m]";
+			}
+			return "[   ?   ]";
+#endif
 		}
 	}
 
